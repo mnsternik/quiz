141b8fab4d0933175d8e08924189a81502f67287
mnsternik <31092045+mnsternik@users.noreply.github.com>
2019-11-22 09:32:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,93 @@
 (function() {
     const questions = [
         question_1 = {
-            name: 'Zasoby, które musza być zamapowane do wystawienia sesji HP to:',
-            a: 'F, I, S' ,
-            b: 'F, I, R',
-            c: 'F, I',
-            d: 'F, I, K',
-            correct: 'F, I'
+            name: 'What would be output of this code: console.log(typeof("0"))?',
+            a: 'Number' ,
+            b: 'String',
+            c: 'False',
+            d: 'Boolean',
+            correct: 'String'
         },
     
         question_2 = {
-            name: 'W przypadku niezgodności lub braku danych w zleceniu na nadanie uprawnień, do kogo zlecenie jest odsyłane z prośba o uzupełnienie:',
-            a: 'Dział Uprawnień',
-            b: 'Dział Bezpieczeństwa',
-            c: 'Helpdesk',
-            d: 'Do osoby zlecającej',
-            correct: 'Dział Bezpieczeństwa'
+            name: 'Which keyword cannot be used to declare a variable?',
+            a: 'Var',
+            b: 'Let',
+            c: 'Function',
+            d: 'Const',
+            correct: 'Function'
         },
     
         question_3 = {
-            name: 'Podczas realizowania zlecenia z ofertowaniem, które z grup mogą być stworzone w jednym pliku:',
-            a: 'PP i PPA4',
-            b: 'PP i Standard', 
-            c: 'PPA4 i Standard',
-            d: 'Dla każdej grupy musi być osobny plik',
-            correct: 'PP i Standard'
+            name: 'What would be output of this code: console.log(1 === "1")?',
+            a: 'true',
+            b: 'false', 
+            c: 'undefined',
+            d: 'Error',
+            correct: 'false'
         },
     
         question_4 = {
-            name: 'Jakimi poświadczeniami logujemy się na serwer WROP3APP24?',
-            a: 'Domeny PL', 
-            b: 'Domeny AIGPRDAS', 
-            c: 'Domeny AIGCRBG', 
-            d: 'Loginem i hasłem dla starych aplikacji',
-            correct: 'Domeny AIGCRBG'
+            name: 'What does method Array.prototype.push()?',
+            a: 'Adds new item to the end of array', 
+            b: 'Removes last item of array', 
+            c: 'Adss new item at the zeroeth index of array', 
+            d: 'Removes item at the zeroeth index of array',
+            correct: 'Adds new item to the end of array'
         },
     
         question_5 = {
-            name: 'Jaka jest godzina graniczna startu I Sprawozdawczości, po przekroczeniu której należy zgłosić opóźnienie w ICBSsie?',
-            a: '6:00',
-            b: '4:30',
-            c: '5:30',
-            d: '5:45',
-            correct: '5:45'
+            name: 'What would be output of this code: console.log(!false && (false || true));?',
+            a: 'false',
+            b: 'undefined',
+            c: 'true',
+            d: 'Error',
+            correct: 'true'
         },
     
         question_6 = {
-            name: 'Na jakim serwerze znajduje się baza Squestion-L_CRU?',
-            a: 'wrop2squestion-l65',
-            b: 'wrop2squestion-l63',
-            c: 'wrop2squestion-l75',
-            d: 'wrop2squestion-l73',
-            correct: 'wrop2squestion-l73'
+            name: 'Which of the following is not a valid JavaScript variable name?',
+            a: '_myVar',
+            b: '2myVar',
+            c: 'my-var',
+            d: 'None of the above',
+            correct: '2myVar'
         },
     
         question_7 = {
-            name: 'Które z wymienionych alarmów w aplikacji DataPumpMonitor należy zgłosić na HelpDesk?',
-            a: 'Uruchomienie CL',
-            b: 'Wiadomość E-mail',
-            c: 'Autoryzacja kart', 
-            d: 'Transakcja analityczna', 
-            correct: 'Uruchomienie CL'
+            name: 'What is corret way co check the legnth of a variable myVar?',
+            a: 'myVar.length()',
+            b: 'myVar.len()',
+            c: 'myVar.length', 
+            d: 'myVar.len', 
+            correct: 'myVar.length'
         },
     
         question_8 = {
-            name: 'Jaka jest wartość procentowa zajętości systemu ASP, po przekroczeniu której należy ja zgłosić  Administratorom AS400?',
-            a: '75%',
-            b: '80%',
-            c: '85%',
-            d: '90%',
-            correct: '80%'
+            name: 'How many iterations this loop will have: for (let i = 3; i < 12; i += 3)',
+            a: '3',
+            b: '4',
+            c: 'infinity',
+            d: '0',
+            correct: '3'
         },
     
         question_9 = {
-            name: 'O której godzinie zaczytywanie jest zapytanie pakietowe BIK?',
-            a: '19:00',
-            b: '19:10',
-            c: '19:30',
-            d: '20:30',
-            correct: '19:10'
+            name: 'Inside which HTML element do we put the JavaScript?',
+            a: 'javascript tag',
+            b: 'js tag',
+            c: 'script tag',
+            d: 'source tag',
+            correct: 'script tag'
         },
     
         question_10 = {
-            name: 'W jakim podsystemie w ICBSie widzimy zalogowanych użytkowników?',
-            a: 'question-INTER',
-            b: 'question-Mquestion-M',
-            c: 'question-BATCH',
-            d: 'question-USERS',
-            correct: 'question-INTER'
+            name: 'How can you add a comment in a JavaScript?',
+            a: '//this is comment',
+            b: '<--this is comment -->',
+            c: '#this is comment',
+            d: '*/this is comment /*',
+            correct: '//this is comment'
         },
     ];
     
@@ -114,6 +114,11 @@
     let quizSection = document.getElementById('quiz-section'); 
     let endSection = document.getElementById('end-section'); 
 
+    let lowScoreImg = document.getElementById('low-score'); 
+    let middleScoreImg = document.getElementById('middle-score'); 
+    let highScoreImg = document.getElementById('high-score'); 
+    let perfectScoreImg = document.getElementById('perfect-score'); 
+
     let checkboxes = [checkbox_1, checkbox_2, checkbox_3, checkbox_4]; 
  
     question.innerHTML = question_1.name; 
@@ -122,7 +127,7 @@
     anwser_3.innerHTML = question_1.c;
     answer_4.innerHTML = question_1.d;
  
-    questionQuantityHeader.innerHTML = `Zaraz odpowiesz na ${questions.length} pytań`;  
+    questionQuantityHeader.innerHTML = `You will answer ${questions.length} questions.`;  
  
     function startQuiz() { 
         startSection.classList.add('disappear'); 
@@ -172,10 +177,17 @@
     }
  
     function getFinalScore() {
-        if (getPointsPercent() < 50) { endPoints = `Twój wynik to ${getPointsPercent()}%. Popraw się!`; }
-        else if (getPointsPercent() < 75) { endPoints = `Twój wynik to ${getPointsPercent()}%. Całkiem nieźle, ale może być lepiej!`; }
-        else if  (getPointsPercent() < 99) { endPoints = `Twój wynik to ${getPointsPercent()}%. Jest dobrze!`; }  
-        else { endPoints = `Twój wynik to ${getPointsPercent()}%. Perfekcyjnie!`; }  
+        if (getPointsPercent() < 50) { 
+            endPoints = `Your score is ${getPointsPercent()}%. Pretty bad!`; 
+            lowScoreImg.classList.add('appear-block');
+        } else if (getPointsPercent() < 75) { 
+            endPoints = `Your score is ${getPointsPercent()}%. Not bad, but could be better!`;
+            middleScoreImg.classList.add('appear-block');
+        } else if  (getPointsPercent() < 99) { 
+            endPoints = `Your score is ${getPointsPercent()}%. Good job!`;
+            highScoreImg.classList.add('appear-block');
+        }  else { endPoints = `Your score is ${getPointsPercent()}%. Perfect!`;
+            perfectScoreImg.classList.add('appear-block'); }  
         endMessage.innerHTML = endPoints;
     }
  
